c2680104bb61af8bd555e944dd56e6dcd60340af
stdweird <stijn.deweirdt@ugent.be>
2014-10-08 22:11:39 +0200

    add address range methods to find specific AR

--- lib/Net/OpenNebula/VNet.pm
+++ lib/Net/OpenNebula/VNet.pm
@@ -89,6 +89,149 @@ sub freear {
     return $self->_ar($id, "free");
 }
 
+# Find the AR matching opts
+# opts are 
+#    ip or MAC : check that they the range (assuming IPv4)
+#    size : match if range is lower or equal the size
+#    If more then opts is defined, all requirements have to fullfill
+# Only one AR is returned (first one wins) 
+# (If no opts are specified, first AR is returned)
+sub get_ar {
+    my ($self, %opts) = @_;
+    $self->verbose("get_ar: no options specified") if (! %opts);
+
+    my %ar_pool = $self->get_ar_pool();
+    return if (! %ar_pool);
+
+    foreach my $id (sort keys %ar_pool) {
+        my $msg = "get_ar AR_ID $id:";
+        my $mac = $ar_pool{$id}->{MAC}->[0];
+        my $ip = $ar_pool{$id}->{IP}->[0];
+        my $size = $ar_pool{$id}->{SIZE}->[0];
+        if(!defined($size)) {
+            $self->debug(2, "$msg AR SIZE undefined, using 1");
+            $size = 1;
+        }
+
+        # use inverse logic because all requirements have to match
+        my $match = 1;
+        
+        # no match if requested size is larger then set one
+        if (exists($opts{size}) && $opts{size} > $size) {
+            $self->debug(2, "$msg NO match: requested size $opts{size} larger then AR Size $size");
+            $match = 0;    
+        }
+        
+        if (exists($opts{mac})) {
+            # get the range
+            if(!defined($mac)) {
+                $self->debug(2, "$msg NO match: requested mac $opts{mac}, but no MAC defined");
+                $match = 0;    
+            } else {
+                # http://www.perlmonks.org/?node_id=440768
+                # Use doubles, no int (bit operators use ints)
+                sub mac_hex2num {
+                    my $mac_hex = shift;
+                    $mac_hex =~ s/://g;
+                    $mac_hex = substr(('0'x12).$mac_hex, -12);
+                    my @mac_bytes = unpack("A2"x6, $mac_hex);
+                    my $mac_num = 0;
+                    foreach (@mac_bytes) {
+                        $mac_num = $mac_num * (2**8) + hex($_);
+                    }
+                    return $mac_num;
+                };
+                
+                my $om = mac_hex2num($opts{mac});
+                my $am = mac_hex2num($mac); 
+                # include boundaries of size?
+                if( ($om < $am) || ($om > ($am + $size -1) )) {
+                    $self->debug(2, "$msg NO match: requested mac $opts{mac} falls outside the range starting with AR MAC $mac and size $size (int mac $om AR MAC $am)");
+                    $match = 0;
+                }
+            }
+        }
+        
+        if (exists($opts{ip})) {
+            # get the range
+            if(!defined($ip)) {
+                $self->debug(2, "$msg NO match: requested ip $opts{ip}, but no IP defined");
+                $match = 0;    
+            } else {
+                # Use doubles, no int (bit operators use ints)
+                sub ip2num {
+                    my $ip = shift;
+                    my $num = 0;
+                    foreach (split(/\./, $ip)) {
+                        $num = $num * (2**8) + $_;
+                    }
+                    return $num;
+                };
+                
+                my $oi = ip2num($opts{ip});
+                my $ai = ip2num($ip); 
+                # include boundaries of size?
+                if( ($oi < $ai) || ($oi > ($ai + $size -1) )) {
+                    $self->debug(2, "$msg NO match: requested ip $opts{ip} falls outside the range starting with AR IP $ip and size $size (int ip $oi AR IP $ai)");
+                    $match = 0;
+                }
+            }
+        }
+        
+        if($match) {
+            $self->debug(1, "$msg found match, returning AR.");
+            return $ar_pool{$id};
+        } else {
+            $self->debug(1, "$msg no match found, continuing.");
+        }
+    }
+    $self->verbose("get_ar: no matching ARs found. Returning undef.");
+    return;
+
+}
+
+# Using same opts as get_ar, returns the ID
+sub get_ar_id {
+    my($self, %opts) = @_;
+    
+    my $arref = $self->get_ar(%opts);
+    if(defined($arref)) {
+        my $id = $arref->{AR_ID}->[0];
+        $self->verbose("get_ar_id: found ID $id");
+        return $id;
+    } else {
+        $self->verbose("get_ar_id: no AR found");
+    }
+}
+
+# Return hash with key=AR_ID, and value the AR instance
+sub get_ar_pool  {
+    my ($self) = @_;
+
+    if ($self->{rpc}->version() < version->new('4.8.0')) {
+        $self->error("AR RPC API new since 4.8.0");
+        return;
+    }
+    
+    $self->_get_info();
+    
+    my %res;
+
+    foreach my $arref (@{$self->{extended_data}->{AR_POOL}}) {
+        my $id = int($arref->{AR}->[0]->{AR_ID}->[0]);
+        $res{$id} = $arref->{AR}->[0];
+    }
+
+    if (%res) {
+        $self->verbose("AR POOL with AR instances", scalar keys %res, " found");
+    } else {
+        $self->verbose("empty AR POOL");
+    };
+    
+    return %res;
+}
+
+
 # Removed since 4.8.0
 sub _leases {
     my ($self, $txt, $mode) = @_;
