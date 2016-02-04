f9ede2e1006611826941614e534b156dd404a97e
stdweird <stijn.deweirdt@ugent.be>
2016-02-04 16:30:48 +0100

    Add user chgrp/addgroup/delgroup

--- lib/Net/OpenNebula/User.pm
+++ lib/Net/OpenNebula/User.pm
@@ -1,6 +1,6 @@
 #
 # (c) Jan Gehring <jan.gehring@gmail.com>
-# 
+#
 # vim: set ts=3 sw=3 tw=0:
 # vim: set expandtab:
 #
@@ -17,7 +17,7 @@ use constant ONERPC => 'user';
 sub name {
    my ($self) = @_;
    $self->_get_info();
-   
+
    # if user NAME is set, use that instead of template NAME
    return $self->{data}->{NAME}->[0] || $self->{data}->{TEMPLATE}->[0]->{NAME}->[0];
 }
@@ -29,9 +29,39 @@ sub create {
    }
    return $self->_allocate([ string => $name ],
                            [ string => $password ],
-                           [ string => $driver ],     
+                           [ string => $driver ],
                           );
 }
 
+# foir current user, apply method to groupid
+sub _do_grp {
+    my ($self, $grp_id, $method) = @_;
+
+    $self->has_id($method) || return;
+
+    $self->debug(1, "$self->{ONERPC} $method id ".$self->id." group id $grp_id");
+    return $self->_onerpc($method,
+                          [ int => $self->id ],
+                          [ int => $grp_id ],
+                          );
+}
+
+# chgrp groupid
+sub chgrp {
+    my ($self, $grp_id) = shift;
+    return $self->_do_grp($grp_id, 'chgrp');
+}
+
+# addgroup groupid
+sub addgroup {
+    my ($self, $grp_id) = shift;
+    return $self->_do_grp($grp_id, 'addgroup');
+}
+
+# delgroup groupid
+sub delgroup {
+    my ($self, $grp_id) = shift;
+    return $self->_do_grp($grp_id, 'delgroup');
+}
 
 1;
