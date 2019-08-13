1ff5e9f9b5a638b66bd4bdfd49ce41bedf10b5d6
Álvaro Simón <varosim@gmail.com>
2019-08-13 13:48:49 +0200

    Minor vmgroup fixes

--- lib/Net/OpenNebula/VMGroup.pm
+++ lib/Net/OpenNebula/VMGroup.pm
@@ -14,10 +14,19 @@ use Net::OpenNebula::RPC;
 push our @ISA , qw(Net::OpenNebula::RPC);
 
 use constant ONERPC => 'vmgroup';
+use constant ONEPOOLKEY => 'VM_GROUP';
 
 sub create {
    my ($self, $tpl_txt) = @_;
    return $self->_allocate([ string => $tpl_txt ]);
 }
+
+sub used {
+   my ($self) = @_;
+   my $vms = $self->_get_info_extended('ROLES');
+   if (defined($vms->[0]->{ROLE}->[0]->{VMS}->[0])) {
+       return 1;
+   }
+};
 
 1;
